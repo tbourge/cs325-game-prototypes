635a12d961964569f0c00b8733492842b3332bbe
tbourge <78454075+tbourge@users.noreply.github.com>
2021-02-09 21:51:06 -0500

    hi

--- digital_assignment1/js/main.js
+++ digital_assignment1/js/main.js
@@ -125,7 +125,7 @@ var Breakout = new Phaser.Class({
 
 const game = new Phaser.Game({
     type: Phaser.AUTO,
-    parent: 'phaser-example',
+    parent: 'game',
     width: 800,
     height: 600,
     scene: [Breakout],
