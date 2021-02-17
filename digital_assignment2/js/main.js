972bb22e9ef8bd1dffc43bd325c4ece3034e2734
tbourge <78454075+tbourge@users.noreply.github.com>
2021-02-17 11:57:27 -0500

    f

--- digital_assignment2/js/main.js
+++ digital_assignment2/js/main.js
@@ -32,6 +32,17 @@ class MyScene extends Phaser.Scene {
         this.load.image('pot9', 'Pot9.png');
         this.load.image('pot10', 'Pot10.png');
         this.load.image('pot11', 'Pot11.png');
+
+        this.load.image('fire0', 'fire0.png');
+        this.load.image('fire1', 'fire1.png');
+        this.load.image('fire2', 'fire2.png');
+        this.load.image('fire3', 'fire3.png');
+        this.load.image('fire4', 'fire4.png');
+        this.load.image('fire5', 'fire5.png');
+        this.load.image('fire6', 'fire6.png');
+        this.load.image('fire7', 'fire7.png');
+
+        this.load.image('man0', 'man0.png');
     }
     
     create() {
@@ -54,9 +65,25 @@ class MyScene extends Phaser.Scene {
             repeat: -1
         });
 
+        this.anims.create({
+            key: 'fire',
+            frames: [
+                { key: 'fire0' },
+                { key: 'fire1' },
+                { key: 'fire2' },
+                { key: 'fire3' },
+                { key: 'fire4' },
+                { key: 'fire5' },
+                { key: 'fire6' },
+                { key: 'fire7', duration: 50 }
+            ],
+            frameRate: 8,
+            repeat: -1
+        });
+
         this.add.sprite(400, 300, 'pot0').play('boiling');
             
-        
+        this.add.sprite(400, 300, 'fire0').play('fire');
         
     }
     
