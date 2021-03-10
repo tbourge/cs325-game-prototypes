26d90ca0fcc62bc633ab89ed0dc128638babdcd2
tbourge <78454075+tbourge@users.noreply.github.com>
2021-03-09 20:14:55 -0500

    Update main.js

--- digital_assignment5/js/main.js
+++ digital_assignment5/js/main.js
@@ -287,11 +287,13 @@ class Pirate extends Phaser.GameObjects.Sprite {
         gameOver = 1;
     }
 
-//    preUpdate() {
-  //      if (this.x > 500) {
-    //        this.lose();
-      //  }
-    //}
+    preUpdate() {
+        super.preUpdate();
+
+        if (this.x > 500) {
+            this.lose();
+        }
+    }
 }
 
 const game = new Phaser.Game({
