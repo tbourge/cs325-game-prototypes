093a7254adcbd6e6942826542ff77ed8c28ee8b7
tbourge <78454075+tbourge@users.noreply.github.com>
2021-03-09 21:19:15 -0500

    Update main.js

--- digital_assignment5/js/main.js
+++ digital_assignment5/js/main.js
@@ -248,6 +248,7 @@ class Ball extends Phaser.GameObjects.Sprite {
 
         scene.physics.world.enableBody(this);
         this.body.velocity.x = -60;
+        this.body.setCircle(50);
     }
 
     preUpdate(time, delta) {
