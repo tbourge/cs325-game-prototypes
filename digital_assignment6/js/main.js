e794c03b0bd06a1288ea6dcf6b26bac4f4c757fb
tbourge <78454075+tbourge@users.noreply.github.com>
2021-04-01 14:30:47 -0400

    a

--- digital_assignment6/js/main.js
+++ digital_assignment6/js/main.js
@@ -93,7 +93,7 @@ class MyScene extends Phaser.Scene {
         timer = this.time.addEvent({ delay: 1000, callback: this.subTime, callbackScope: this, repeat: -1, paused: true });
 
         this.text = this.add.text(100, 450, "You will have 3 seconds to memorize the board\n Click START when ready", { fontSize: 25 }).setColor('#ffffff');
-        this.timeText = this.add.text(550, 10, "Time: 0", { fontSize: 10 }).setColor('#ffffff');
+        this.timeText = this.add.text(570, 10, "Time: 0", { fontSize: 10 }).setColor('#ffffff');
 
         start = this.add.sprite(400, 300, 'startButton').setInteractive();
 
