a57920e7aed6eae0f5259896e1a9201d7047c06b
tbourge <78454075+tbourge@users.noreply.github.com>
2021-04-15 17:48:22 -0400

    q

---
+++ digital_assignment8/js/main.js
@@ -0,0 +1,42 @@
+import "./phaser.js";
+
+// You can copy-and-paste the code from any of the examples at https://examples.phaser.io here.
+// You will need to change the `parent` parameter passed to `new Phaser.Game()` from
+// `phaser-example` to `game`, which is the id of the HTML element where we
+// want the game to go.
+// The assets (and code) can be found at: https://github.com/photonstorm/phaser3-examples
+// You will need to change the paths you pass to `this.load.image()` or any other
+// loading functions to reflect where you are putting the assets.
+// All loading functions will typically all be found inside `preload()`.
+
+// The simplest class example: https://phaser.io/examples/v3/view/scenes/scene-from-es6-class
+
+class MyScene extends Phaser.Scene {
+    
+    constructor() {
+        super();
+
+        this.board;
+    }
+    
+    preload() {
+        this.load.image( 'space', 'assets/Square.png' );
+    }
+    
+    create() {
+        this.board = this.physics.add.group({ key: 'space', repeat: 13, setXY: { x: 32, y: 100, stepX: 62 } });
+    }
+    
+    update() {
+
+    }
+}
+
+const game = new Phaser.Game({
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 600,
+    scene: MyScene,
+    physics: { default: 'arcade' },
+    });
